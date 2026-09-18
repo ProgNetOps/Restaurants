9f8c05915656ffe92731c4bc28e465e3ef869a72
Seyi <suberuseyi@gmail.com>
2026-09-18 22:50:41 +0100

    Solution setup for clean architecture implemented

---
+++ Restaurants.Domain/Entities/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace Restaurants.Domain.Entities;
+
+public class Address
+{
+    //No Id because we do not intend to create a table for address in the database
+    public string? City { get; set; }
+    public string? Street { get; set; }
+    public string? PostalCode { get; set; }
+}
